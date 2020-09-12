12f772f6a9a8bb92b90f959e72cfba0120f30a52
Jan Kollars <kollars.jan@gmail.com>
2020-09-12 11:52:31 +0200

    remove hardcoded coordinates from script

--- public/geocoder.py
+++ public/geocoder.py
@@ -12,37 +12,12 @@ with open(f"{current_folder}/caritas.csv", mode="r") as csv_file:
         data.append(row)
 
 for address in data:
-    def add_coordinates(toAdd):
+    def set_coordinates(toAdd):
             address["lon"] = toAdd["lon"]
             address["lat"] = toAdd["lat"]
 
-    if address["street"] == "Am Hofgartl 1":
-        add_coordinates({
-            "lon": 16.464477,
-            "lat": 48.1551172
-        })
-    elif address["street"] == "Baron-Karl-Gasse 7":
-        add_coordinates({
-            "lon": 16.3526552,
-            "lan": 48.1538041
-        })
-    elif address["street"] == "Blebanngasse/Schrutkagasse":
-        add_coordinates({
-            "lon": 16.2768665,
-            "lat": 48.1838004
-        })
-    elif address["street"] == "Meißauergasse 2a/1":
-        add_coordinates({
-            "lon": 16.438936,
-            "lat": 48.248264
-        })
-    elif address["street"] == "Endresstraße 59/Rudolf-Zellergasse":
-        add_coordinates({
-            "lon": 16.283129,
-            "lat": 48.147326
-        })
 
-    else:
+    if not (address["lon"] and address["lat"]):
         response = requests.get(
                 f"https://nominatim.openstreetmap.org/search.php?street={address['street']}&city={address['city']}&state={address['state']}&country=Austria&polygon_geojson=1&format=jsonv2"
         )
@@ -52,11 +27,11 @@ for address in data:
             house_found = False
             if result["type"] == "house":
                 house_found = True
-                add_coordinates(result)
+                set_coordinates(result)
             if house_found:
                 break
             else:
-                add_coordinates(response_data[0])
+                set_coordinates(response_data[0])
 
 with open(f"{current_folder}/caritas_coded.csv", mode="w") as csv_file:
     writer = csv.DictWriter(
